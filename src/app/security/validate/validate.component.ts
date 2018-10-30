acee4716dac5d5a4b55e5b004ba14811d6fa4cdc
patsaf <patrycja.saferna@gmail.com>
2018-10-30 22:45:46 +0100

    Update overlooked oomponent

--- src/app/security/validate/validate.component.ts
+++ src/app/security/validate/validate.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {LoginService} from '../../services/login.service';
 import {ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-validate',
@@ -9,13 +10,21 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ValidateComponent {
 
-  password: string;
+  form: FormGroup;
+  password: FormControl;
 
   constructor(private loginService: LoginService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute) {
+    this.password = new FormControl('', [
+      Validators.required
+    ]);
+    this.form = new FormGroup({
+      "password": this.password
+    });
+  }
 
   submit() {
-    this.loginService.validateUser(this.route.snapshot.params['id'], this.password);
+    this.loginService.validateUser(this.route.snapshot.params['id'], this.form.get('password').value);
   }
 
 
